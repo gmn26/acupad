4ca353032f8b1368eb0a2da4c402a75699f31822
gmn26 <ghazynovrial@gmail.com>
2024-12-16 22:55:31 +0700

    update : add new preferences for UI purpose

--- lib/utils/preferences_util.dart
+++ lib/utils/preferences_util.dart
@@ -10,7 +10,16 @@ class PreferencesUtils extends GetxService {
   }
 
   bool? getCheckedIn() => _preferences.getBool('checked_in');
+  String? getCheckInTime() => _preferences.getString('checked_in_time');
+  String? getLastNotif() => _preferences.getString('last_notif');
+  String? getNextNotif() => _preferences.getString('next_notif');
 
   Future<bool> setCheckedIn(bool checkedIn) =>
       _preferences.setBool('checked_in', checkedIn);
+  Future<bool> setCheckedInTime(String checkedInTime) =>
+      _preferences.setString('checked_in_time', checkedInTime);
+  Future<bool> setLastNotif(String lastNotif) =>
+      _preferences.setString('last_notif', lastNotif);
+  Future<bool> setNextNotif(String nextNotif) =>
+      _preferences.setString('next_notif', nextNotif);
 }
